8c217e6f4109313c6ca4d946b0c50ed011a173ce
DiracSpace <deleon.martinez.16138@itsmante.edu.mx>
2020-04-22 21:10:19 -0400

    Cpp compiler with GCC linking

---
+++ Asriel/src/Compile/compiler.java
@@ -0,0 +1,19 @@
+package Compile;
+
+/**
+ *
+ * @author DiracSpace & Flambo
+ */
+
+public class compiler {
+    
+    public String filepathC;
+    
+    public compiler(String filepath) {
+        this.filepathC = filepath;
+    }
+    
+    public String Compile(){
+        return filepathC;
+    }
+}
